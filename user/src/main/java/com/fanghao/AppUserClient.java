37854daee959d82fe88c68511985a380e87535c9
faicm <2725232638@qq.com>
2020-08-11 23:50:14 +0800

    学习

---
+++ user/src/main/java/com/fanghao/AppUserClient.java
@@ -0,0 +1,12 @@
+package com.fanghao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppUserClient {
+    public static void main(String[] args) {
+
+        SpringApplication.run(AppUserClient.class);
+    }
+}
