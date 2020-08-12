b08fbab961757c9836c67d1434eeaf69bc51c987
faicm <2725232638@qq.com>
2020-08-13 00:22:48 +0800

    学习

--- user/src/main/java/com/fanghao/controller/UserController.java
+++ user/src/main/java/com/fanghao/controller/UserController.java
@@ -1,10 +1,30 @@
 package com.fanghao.controller;
 
+import com.fanghao.util.R;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class UserController {
 
     @Autowired
     RestTemplate restTemplate;
+
+    private static final String  POWER_URL="http://SERVER-POWER";
+    private static final String  ORDER_URL="http://SERVER-ORDER";
+
+    @RequestMapping("/getUser.do")
+    public R getUser(){
+        Map<String,Object> map = new HashMap<>();
+        map.put("key","user");
+        return R.success("返回成功",map);
+    }
+
+    @RequestMapping("/getOrder.do")
+    public R getOrder(){
+        return R.success("操作成功",restTemplate.getForObject(ORDER_URL+"/getOrder.do",Object.class));
+    }
 }
