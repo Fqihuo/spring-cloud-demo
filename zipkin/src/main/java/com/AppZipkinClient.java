72084e1f03ec820986329da65fee21049eb229ff
Fqihuo <2725232638@qq.com>
2020-09-12 15:16:53 +0800

    学习

---
+++ zipkin/src/main/java/com/AppZipkinClient.java
@@ -0,0 +1,9 @@
+package com;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import zipkin.server.internal.EnableZipkinServer;
+
+@SpringBootApplication
+@EnableZipkinServer
+public class AppZipkinClient {
+}
