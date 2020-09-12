f57988a8bf0563dc4b46d3daa466157ebf9f1327
Fqihuo <2725232638@qq.com>
2020-09-12 15:35:39 +0800

    学习

--- zipkin/src/main/java/com/AppZipkinClient.java
+++ zipkin/src/main/java/com/AppZipkinClient.java
@@ -1,9 +1,15 @@
 package com;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import zipkin.server.internal.EnableZipkinServer;
 
 @SpringBootApplication
 @EnableZipkinServer
+@EnableEurekaClient
 public class AppZipkinClient {
+    public static void main(String[] args) {
+        SpringApplication.run(AppZipkinClient.class);
+    }
 }
